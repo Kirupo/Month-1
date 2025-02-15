0489261fced18ceb6602a5817c001129306cde81
Kirupo <kirubelg217@gmail.com>
2025-02-15 11:19:02 +0300

    Create Write a function that sums numbers in a list.py

---
+++ week 2/Write a function that sums numbers in a list.py
@@ -0,0 +1,9 @@
+#function that sums numbers in a list
+#The main code
+def sum_list(numbers):
+    return sum(numbers)
+
+# Example usage
+nums = [1, 2, 3, 4, 5]
+print(sum_list(nums))  
+# Output: 15
