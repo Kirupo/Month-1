14ac1bdeaa1bf6a55dc7f98ef527f81dc6e0e8f4
Kirupo <kirubelg217@gmail.com>
2025-02-15 11:25:51 +0300

    Create Write a program to find the largest number in a list.py

---
+++ week 2/Write a program to find the largest number in a list.py
@@ -0,0 +1,8 @@
+#Write a program to find the largest number in a list.
+def find_largest(numbers):
+  return max(numbers)
+
+#Example usage
+nums = [100,123,675,987,1253]
+print("The largest number is:", find_largest(nums))
+#Output The largest number is: 1253 
